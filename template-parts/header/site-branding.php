f0df0e950b87c918713cf298778b16bffee026f6
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-02-19 09:13:35 +0100

    initial commit

---
+++ template-parts/header/site-branding.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Header Main
+ *
+ * @version 1.0
+ * @package GT Drive
+ */
+?>
+
+<div id="logo" class="site-branding">
+
+	<?php the_custom_logo(); ?>
+	<?php gt_drive_site_title(); ?>
+	<?php gt_drive_site_description(); ?>
+
+</div><!-- .site-branding -->
