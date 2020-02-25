37eaa5d3d9ffc0e6a05e5421b5fca0e456aeebba
Thomas Weichselbaumer <kontakt@themecoder.de>
2020-02-25 16:29:56 +0100

    added full width and no title page templates

---
+++ template-parts/page/content-no-title.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template used for displaying page content without a page title.
+ *
+ * @version 1.0
+ * @package GT Drive
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="entry-content">
+
+		<?php the_content(); ?>
+		<?php wp_link_pages(); ?>
+
+	</div><!-- .entry-content -->
+
+</article>
